775353b503f32d86cf5343d79e55176af3c8f904
Yogeshbairwa <ybairwa786@gmail.com>
2024-04-08 08:25:14 +0530

    updaes

---
+++ src/DateCounterV1.js
@@ -0,0 +1,72 @@
+import { useReducer, useState } from "react";
+
+
+function reducer(state, action) {
+  console.log(state, action);
+  return state + action;
+}
+
+function DateCounter() {
+
+  const [count, dispatch] = useReducer(reducer, 0)
+
+  const [step, setStep] = useState(1);
+
+
+  const date = new Date("june 21 2027");
+  date.setDate(date.getDate() + count);
+
+  const dec = function () {
+    dispatch(-1)
+    // setCount((count) => count - 1);
+    // setCount((count) => count - step);
+  };
+
+  const inc = function () {
+    dispatch(1)
+    // setCount((count) => count + 1);
+    // setCount((count) => count + step);
+  };
+
+  const defineCount = function (e) {
+    // setCount(Number(e.target.value));
+    dispatch(Number(e.target.value));
+  };
+
+  const defineStep = function (e) {
+    // setStep(Number(e.target.value));
+  };
+
+  const reset = function () {
+    // setCount(0);
+    // setStep(1);
+  };
+
+  return (
+    <div className="counter">
+      <div>
+        <input
+          type="range"
+          min="0"
+          max="10"
+          value={step}
+          onChange={defineStep}
+        />
+        <span>{step}</span>
+      </div>
+
+      <div>
+        <button onClick={dec}>-</button>
+        <input value={count} onChange={defineCount} />
+        <button onClick={inc}>+</button>
+      </div>
+
+      <p>{date.toDateString()}</p>
+
+      <div>
+        <button onClick={reset}>Reset</button>
+      </div>
+    </div>
+  );
+}
+export default DateCounter;
